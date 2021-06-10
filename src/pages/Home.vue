8792564f6c6a5beb65a8eb49058f9f3f6e1c9c9b
Aminul Islam <me@aminul.net>
2021-06-10 10:40:43 +0600

    update message

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="bg-gray-100 py-2 min-h-screen">
     <div class="container mx-auto">
-      <div class="w-96 bg-white border-l-4 border-t-4 border-blue-600 p-10 mx-auto">
+      <div class="w-96 bg-white border-l-4 border-t-4 border-blue-600 p-10 mx-auto rounded-tl">
         Hello {{ profile.first_name }}, View your orders <router-link class="border-b-2 border-red-700" :to="{name: 'Orders'}">here</router-link>.
       </div>
 
-      <div class="w-96 bg-white border-l-4 mt-10 border-t-4 border-green-600 p-10 mx-auto">
-        Hi folks, I'm trying to build a freaking app to track my Workdays (AKA: Kormodibosh). I build this app because I'm jobless now and have enough time to learn something new.
+      <div class="w-96 bg-white border-l-4 mt-10 border-t-4 border-green-600 p-10 mx-auto rounded-tl">
+        Hi folks, I'm trying to build a freaking app to track my Workdays (AKA: Kormodibosh).
       </div>
 
-      <div class="w-96 bg-white border-l-4 mt-10 border-t-4 border-red-500 p-10 mx-auto">
+      <div class="w-96 bg-white border-l-4 mt-10 border-t-4 border-red-500 p-10 mx-auto rounded-tl">
         Note: When you are login using your evaly username and password, All information will be stored in your browser and all safe as your computer. You can see the source linked in the footer.
       </div>
     </div>
